7bcbe41fdd85041596a30dd49a3d605df5d237e5
MohsenSami <mohsensami88@gmail.com>
2023-01-12 12:02:16 +0330

    server.js websocket added to project

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const WebSocket = require('ws');
+
+const PORT = 5555;
+
+const wsServer = new WebSocket.Server({
+    port: PORT
+});
+
+wsServer.on('connection', function (socket) {
+    console.log("A client just connected");
+
+    socket.on('message', function (msg) {
+        console.log("Received message from client: "  + msg);
+
+        wsServer.clients.forEach(function (client) {
+            client.send("Someone said: " + msg);
+        });
+
+    });
+
+    socket.on('close', function () {
+        console.log('Client disconnected');
+    })
+
+});
+
+console.log( (new Date()) + " Server is listening on port " + PORT);
